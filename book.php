c94097d63ce7977f721d5e5125682a0b9ec89c59
MarcoB <mrc.bsllt@gmail.com>
2021-02-05 20:59:33 +0100

    stampato libro utilizzando una classe predefinita

--- book.php
+++ book.php
@@ -2,13 +2,15 @@
   class Book {
     public $title;
     public $author;
+    public $cover;
     public $plot;
     public $year;
     public $vote;
 
-    function __construct($title, $author, $vote = 0) {
+    function __construct($title, $author, $year, $vote = 0) {
       $this->title = $title;
       $this->author = $author;
+      $this->year = $year;
     }
 
   }
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
   include __DIR__."/book.php";
 
-  $book1 = new Book ("Le Asagiatrici", "Rossella Postorino");
-
+  $book1 = new Book ("Le Asagiatrici", "Rossella Postorino", 2018);
+  $book1->plot = "Rosa Sauer, la protagonista, diventa assaggiatrice ufficiale dei cibi di Adolf Hitler, durante la Seconda guerra mondiale. ... Il marito, Gregor, è partito per la guerra e sta combattendo sul fronte russo. Dopo una settimana dal suo arrivo viene reclutata per diventare un'assaggiatrice di Hitler.";
+  var_dump($book1->vote);
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +13,15 @@
     <title>php-oop-1</title>
   </head>
   <body>
-    
+
+    <div class="book" style="width:50%">
+      <h2><?= $book1->title."<small style='margin-left:20px; font-size:15px'>(".$book1->author.")</small>"; ?></h2>
+      <p><?= $book1->plot; ?></p>
+      <small><strong><?= $book1->year; ?></strong></small>
+      <p><strong>Voto:</strong> <?= $book1->vote; ?></p>
+
+
+    </div>
 
 
   </body>
